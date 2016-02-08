0a68f6311220aa2649a862901ab9c43ee2b177bd
NicolasLoew <ympker@web.de>
2016-02-08 15:02:50 +1030

    Update otsetup.sh

--- otsetup.sh
+++ otsetup.sh
@@ -1,6 +1,21 @@
-wget https://raw.githubusercontent.com/NicolasLoew/vps/master/panelsetup.sh
-sh panelsetup.sh
-wget https://raw.githubusercontent.com/NicolasLoew/vps/master/tfsauto.sh
-sh tfsauto.sh
-wget https://raw.githubusercontent.com/NicolasLoew/vps/master/znotesetup.sh
-sh znotesetup.sh
+read -r -p "Do you want to install the webpanel? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+    wget https://raw.githubusercontent.com/NicolasLoew/vps/master/panelsetup.sh
+    sh panelsetup.sh
+ fi
+read -r -p "Do you want to compile latest TFS? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+    wget https://raw.githubusercontent.com/NicolasLoew/vps/master/tfsauto.sh
+    sh tfsauto.sh
+ fi
+ read -r -p "Are you sure? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+  wget https://raw.githubusercontent.com/NicolasLoew/vps/master/znotesetup.sh
+  sh znotesetup.sh  
+ fi
+
+
+
