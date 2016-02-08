6e4d7c5eab03319774dc5161a9ee2a314b96b1a6
NicolasLoew <ympker@web.de>
2016-02-08 16:47:53 +1030

    Update znotesetup.sh

--- znotesetup.sh
+++ znotesetup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /var/www/html
 #Setting up ZnoteAAC
-git clone 
-https://github.com/Znote/ZnoteAAC.git
+git clone https://github.com/Znote/ZnoteAAC.git
 #Moving ZnoteAAC to html
 mv * ../
