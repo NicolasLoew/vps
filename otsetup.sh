bd103462e54695c2819a686de0d2436478f41746
NicolasLoew <ympker@web.de>
2016-02-08 14:52:41 +1030

    Create otsetup.sh

---
+++ otsetup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/NicolasLoew/vps/master/panelsetup.sh
+sh panelsetup.sh
+wget https://raw.githubusercontent.com/NicolasLoew/vps/master/tfsauto.sh
+sh tfsauto.sh
+wget https://raw.githubusercontent.com/NicolasLoew/vps/master/znotesetup.sh
+sh znotesetup.sh
