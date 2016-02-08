834ba8ea5220bbe6a7151556933a0d37219fc565
NicolasLoew <ympker@web.de>
2016-02-09 02:17:15 +1030

    Update tfsauto.sh

--- tfsauto.sh
+++ tfsauto.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get install luajit
 apt-get install sudo
 sudo apt-get install git cmake build-essential liblua5.2-dev libgmp3-dev libmysqlclient-dev libboost-system-dev
+apt-get update
 apt-get upgrade
 git clone https://github.com/otland/forgottenserver.git
 cd forgottenserver
@@ -10,6 +11,7 @@ mkdir build && cd build
  read -r -p "If you dont want to use latest TFS sources but instead use your own sources to compile please exchange them with those in the build folder before you continue. Continue? [Y/n]" response
  response=${response,,} # tolower
  if [[ $response =~ ^(yes|y| ) ]]; then
+  cd build
   cmake ..
   make
   mv tfs ..
