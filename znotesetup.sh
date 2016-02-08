b8fdc82cd95dddfa2d56e1090f99a25bb17216bc
NicolasLoew <ympker@web.de>
2016-02-08 21:31:58 +1030

    Update znotesetup.sh

--- znotesetup.sh
+++ znotesetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /var/www/html
-#Setting up ZnoteAAC
 sudo apt-get install php5
 sudo apt-get install php5-curl
+cd /var/www/html
+#Setting up ZnoteAAC
 git clone https://github.com/Znote/ZnoteAAC.git
 cd ZnoteAAC
 #Moving ZnoteAAC to html
