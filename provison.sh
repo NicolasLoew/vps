b6207b05409b274b068a7cf84ea2f8c3c0822bd6
NicolasLoew <ympker@web.de>
2016-02-15 23:11:30 +1030

    Update provison.sh

--- provison.sh
+++ provison.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#hostnameandhostschmod0644
 {
 cd /etc/init.d
 wget https://raw.githubusercontent.com/NicolasLoew/vps/master/hostnameubuntu.sh
@@ -7,5 +8,5 @@ cd /etc/profile.d
 wget https://raw.githubusercontent.com/NicolasLoew/vps/master/hostnameubuntu.sh
 chmod +x hostnameubuntu.sh
 cd /etc
-sed 's/^server-provider/vps-unlimited/' hosts.txt > file2.txt
+sed 's/^server-provider/vps-unlimited/' hosts > hosts
 } &> /dev/null
