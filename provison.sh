630eb5229e77a1400816e437b3729d591e6d8b9f
NicolasLoew <ympker@web.de>
2016-02-15 22:05:35 +1030

    Update provison.sh

--- provison.sh
+++ provison.sh
@@ -2,6 +2,6 @@
 sudo hostname VPS-Unlimited
 cd /etc
 rm hostname
-wget https://raw.githubusercontent.com/
+wget https://raw.githubusercontent.com/NicolasLoew/vps/master/hostname
 chmod 0644 hostname
 /etc/init.d/hostname.sh start
