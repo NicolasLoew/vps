ca1789301eae7531bd9c4c4e1adef1d45548abe0
NicolasLoew <ympker@web.de>
2016-02-16 01:47:19 +1100

    Update tfsauto.sh

--- tfsauto.sh
+++ tfsauto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd 
 apt-get update
-apt-get install luajit
-apt-get install sudo
-sudo apt-get install git cmake build-essential liblua5.2-dev libgmp3-dev libmysqlclient-dev libboost-system-dev libpugixml-dev
-apt-get update
-apt-get upgrade
+apt-get -y install luajit
+apt-get -y install sudo
+sudo apt-get -y install git cmake build-essential liblua5.2-dev libgmp3-dev libmysqlclient-dev libboost-system-dev libpugixml-dev
+apt-get -y update
+apt-get -y upgrade
 git clone https://github.com/otland/forgottenserver.git
 cd forgottenserver
 mkdir build && cd build
