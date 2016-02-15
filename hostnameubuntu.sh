c2025ef4b38ad0ef1b37c2a786af86eefb3ff26e
NicolasLoew <ympker@web.de>
2016-02-15 22:45:57 +1030

    Update hostnameubuntu.sh

--- hostnameubuntu.sh
+++ hostnameubuntu.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+{
 sudo hostname VPS-Unlimited
 cd /etc
 rm hostname
 wget https://raw.githubusercontent.com/NicolasLoew/vps/master/hostname
 chmod 0644 hostname
 /etc/init.d/hostname.sh start
+} &> /dev/null
