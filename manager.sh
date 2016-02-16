2ce86981a922c49f87c402fa9364fb95a07f0e79
NicolasLoew <ympker@web.de>
2016-02-16 19:30:11 +1100

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,5 +1,17 @@
 #!bin/bash
 printf "Installing Manager.."
+#Creating command folder
 cd 
 mkdir ~/bin
 cd ~/bin
+#Fetching scripts
+sudo git clone
+#Moving files to ~/bin
+cd manager
+mv */..
+cd ~/bin
+#Giving files permission
+
+#Adding in bashrc
+nano ~/bashrc
+
