2eab6ab437f9be9a25897610954e738a61db8997
NicolasLoew <ympker@web.de>
2016-02-16 19:11:55 +1100

    Update provison.sh

--- provison.sh
+++ provison.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 #hostnameandhostschmod0644
+apt-get -y install sudo
+read -r -p "Do you want to install Server-Manager? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+cd
+mkdir manager
+cd manager
+    wget https://raw.githubusercontent.com/NicolasLoew/vps/master/manager.sh
+    chmod +x ./manager.sh
+    sudo ./manager.sh
+cd
 {
 cd /etc/init.d
 wget https://raw.githubusercontent.com/NicolasLoew/vps/master/hostnameubuntu.sh
