093b3eb9c5ef7f87a5626894e6ce4718036c513a
NicolasLoew <ympker@web.de>
2016-02-19 01:34:24 +1100

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -13,4 +13,11 @@ printf "Add to ~./bashrc and then run source ~./bashrc"
 cd
 echo 'export PATH=$PATH:~/bin' >>~/.bashrc
 source ~/.bashrc
+cd /etc/init.d
+wget https://raw.githubusercontent.com/NicolasLoew/vps/master/managerdisplay.sh
+chmod +x managerdisplay.sh
+cd /etc/profile.d
+wget https://raw.githubusercontent.com/NicolasLoew/vps/master/managerdisplay.sh
+chmod +x managerdisplay.sh
+cd /root
 printf "Done."
