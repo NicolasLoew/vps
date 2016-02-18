36241486e7484cd772b670c97595bef174613e23
NicolasLoew <ympker@web.de>
2016-02-18 23:50:10 +1100

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,21 +1,15 @@
 #!bin/bash
 printf "Installing Manager.."
-#Creating command folder
-cd 
-mkdir ~/bin
 #Installing git
 sudo apt-get -y install git
-cd ~/bin
 #Fetching scripts
-sudo git clone https://github.com/NicolasLoew/manager.git
-#Moving files to ~/bin
-cd manager
-mv * ../
+sudo git clone https://github.com/NicolasLoew/bin.git
 #Giving files permission
 cd 
 chmod -R 777 ~/bin
 #Adding in bashrc "export PATH=$PATH:~/bin"
 #Then run "source ~./bashrc"
 printf "Add to ~./bashrc and then run source ~./bashrc"
-cd 
-nano . ~/.bashrc
+cd
+echo 'export PATH=/usr/local/bin:$PATH' >>~/.bash_profile
+source ~./bashrc
