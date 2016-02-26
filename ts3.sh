71efbfebe4889baa9ebf730851a9a56846436a09
NicolasLoew <ympker@web.de>
2016-02-26 23:36:01 +1100

    Update ts3.sh

--- ts3.sh
+++ ts3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get -y update
 wget http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd64-3.0.11.2.tar.gz
+tar xzf teamspeak3-server_linux-amd64-3.0.11.2.tar.gz
 sudo adduser --disabled-login teamspeak
 #sudo mv teamspeak3-server_linux-amd64 /usr/local/teamspeak
 #sudo chown -R teamspeak:teamspeak /usr/local/teamspeak
