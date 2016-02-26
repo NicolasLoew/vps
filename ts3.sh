c16dfa759109f1f71b482df64acd5114705ca890
NicolasLoew <ympker@web.de>
2016-02-26 19:24:12 +1100

    Update ts3.sh

--- ts3.sh
+++ ts3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://dl.4players.de/ts/releases/3.0.11.3/teamspeak3-server_linux-amd64-3.0.11.3.tar.gz 
-tar -xzvf teamspeak3-server_linux-amd64-3.0.11.3.tar.gz 
+wget http://dl.4players.de/ts/releases/3.0.12.1/teamspeak3-server_linux-amd64-3.0.12.1.tar.gz 
+tar -xzvf teamspeak3-server_linux-amd64-3.0.12.1.tar.gz 
 cd teamspeak3-server_linux-amd64/ 
 ./ts3server_startscript.sh start
