34223cfff37fef0ccf93955949660e5a1365b4a3
NicolasLoew <ympker@web.de>
2016-03-08 18:42:05 +1300

    Update provison.sh

--- provison.sh
+++ provison.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #hostnameandhostschmod0644
-apt-get -y install sudo
-wget --no-check-certificate https://raw.githubusercontent.com/NicolasLoew/vps/master/manager.sh
-    chmod +x manager.sh
-    sh manager.sh
+#apt-get -y install sudo
+#wget --no-check-certificate https://raw.githubusercontent.com/NicolasLoew/vps/master/manager.sh
+    #chmod +x manager.sh
+   # sh manager.sh
 {
 cd /etc/init.d
 wget https://raw.githubusercontent.com/NicolasLoew/vps/master/hostnameubuntu.sh
