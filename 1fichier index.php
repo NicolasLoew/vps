dc968f9dfeb681209966e5be791de81863b8ac16
Nicolas Loew <ympker@web.de>
2017-06-24 08:26:55 +0200

    Create 1fichier index.php

---
+++ 1fichier index.php
@@ -0,0 +1,20 @@
+<?php
+if(!isset($_GET['a'])) {
+	die('<form action="" method="get"><input type="text" name="a"/><input type="submit"/></form>');
+}
+$url = "https://1fichier.com/?" . urlencode($_GET['a']) . "?auth=1&inline";
+stream_context_set_default(array(
+			'http' => array(
+				'method' => 'GET',
+				'header' => 'Authorization: Basic ' . '[BASE64 encoded login credentials. Format: "user:pass" (without quotes)]'
+				)
+			));
+$headers = get_headers($url, 1);
+if ($headers !== false && isset($headers['Location'])) {
+	$loc = $headers['Location'];
+	$loc = str_replace("https://", "http://", $loc);
+	header('Location: ' . $loc, true, 302);
+} else {
+	echo "Failed to unarchive file!";
+}
+?>
