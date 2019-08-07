17cd62975ac46c5c0fb0d126e6881aaa9a743c0c
Nicolas Loew <ympker@web.de>
2019-08-07 17:51:31 +0200

    Create cabalbot.py

---
+++ cabalbot.py
@@ -0,0 +1,13 @@
+import time
+import pyautogui
+
+pyautogui.click(950, 575)
+for k in range(0,10)
+pyautoguy.middleClick(950, 575)
+pyautogui.rightClick(650, 320)
+time.sleep(5)
+
+#https://www.youtube.com/watch?v=L1CXGsg-Xh4
+#PyAutoGui
+#PyCharm
+#MousePos (for mouse positon)
