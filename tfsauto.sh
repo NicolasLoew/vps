a3dc5ad7c02acbed5898bcde0a41d4930f99fd62
Nicolas Loew <ympker@web.de>
2019-08-19 00:22:56 +0200

    Update tfsauto.sh

--- tfsauto.sh
+++ tfsauto.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "This script will compile TFS and install a MySQL server for Ubuntu 18.04 64bit LTS."
+echo "This script will compile TFS and install a LAMP stack for Ubuntu 18.04 64bit LTS."
 echo "This script needs to be executed as a user with sudo privileges.
 cd 
 echo "Updating the system.."
 apt-get -y update
-echo "Installing required software.."
 apt-get -y install sudo
+echo "Installing LAMP Stack.."
+sudo apt-get -y install apache2
+sudo apt-get -y install mysql-server
+sudo apt-get -y install php libapache2-mod-php php-mysql
+echo "Installing required softwarefor TFS.."
 sudo apt-get -y install luajit
 sudo apt-get -y install git cmake build-essential liblua5.2-dev libgmp3-dev libmysqlclient-dev libboost-system-dev libboost-iostreams-dev libboost-filesystem-dev libpugixml-dev libcrypto++-dev
 sudo apt-get -y update
