de82e64dd52e34feaebd362896541824df16fff7
Nicolas Loew <ympker@web.de>
2019-08-19 00:01:20 +0200

    Update tfsauto.sh

--- tfsauto.sh
+++ tfsauto.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+echo "This script will compile TFS and install a MySQL server for Ubuntu 18.04 64bit LTS."
+echo "This script needs to be executed as a user with sudo privileges.
 cd 
+echo "Updating the system.."
 apt-get -y update
+echo "Installing required software.."
 apt-get -y install sudo
 sudo apt-get -y install luajit
 sudo apt-get -y install git cmake build-essential liblua5.2-dev libgmp3-dev libmysqlclient-dev libboost-system-dev libboost-iostreams-dev libboost-filesystem-dev libpugixml-dev libcrypto++-dev
 sudo apt-get -y update
+echo "Downloading and compiling TFS..Please wait.."
 sudo git clone --recursive https://github.com/otland/forgottenserver.git
 cd forgottenserver
-mkdir build && cd build
- read -r -p "If you dont want to use latest TFS sources but instead use your own sources to compile please exchange them with those in the build folder before you continue. Continue? [Y/n]" response
- response=${response,,} # tolower
- if [[ $response =~ ^(yes|y| ) ]]; then
-  cmake ..
-  make
-  mv tfs ..
-   fi
+sudo mkdir build && cd build
+sudo cmake ..
+sudo make
+sudo mv tfs ..
 cd
-cd tfs
-echo "Done. Make sure to adjust settings in config before starting the server."
-echo "To start the server use 'cd' to navigate to your home directory then start it using './tfs'"
+cd ..
+echo "Done. Make sure to adjust config before starting the server."
+echo "To start the server use 'cd' to navigate to your home directory."
+echo "You can launch it from there using './tfs'"
