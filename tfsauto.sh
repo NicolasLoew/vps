13283add7de13300a952785ef51df41196609d2a
Nicolas Loew <ympker@web.de>
2019-08-19 22:07:12 +0200

    Update tfsauto.sh

--- tfsauto.sh
+++ tfsauto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "This script will compile TFS and install a LAMP stack for Ubuntu 18.04 64bit LTS."
-echo "This script needs to be executed as a user with sudo privileges.
+#echo "This script will compile TFS and install a LAMP stack for Ubuntu 18.04 64bit LTS."
+#echo "This script needs to be executed as a user with sudo privileges.
 cd 
 echo "Updating the system.."
 apt-get -y update
 apt-get -y install sudo
-echo "Installing LAMP Stack.."
+#echo "Installing LAMP Stack.."
 sudo apt-get -y install apache2
 sudo apt-get -y install mysql-server
 sudo mysql -e "CREATE USER 'otsuser'@'localhost' IDENTIFIED BY 'password';"
@@ -13,11 +13,11 @@ sudo mysql -e "CREATE DATABASE otsdb;"
 sudo mysql -e "GRANT ALL PRIVILEGES ON otsdb.* TO 'otsuser'@'localhost';"
 sudo apt-get -y install php libapache2-mod-php php-mysql
 sudo service apache2 start
-echo "Installing required software.."
+#echo "Installing required software.."
 sudo apt-get -y install luajit
 sudo apt-get -y install git cmake build-essential liblua5.2-dev libgmp3-dev libmysqlclient-dev libboost-system-dev libboost-iostreams-dev libboost-filesystem-dev libpugixml-dev libcrypto++-dev
 sudo apt-get -y update
-echo "Downloading and compiling TFS..Please wait.."
+#echo "Downloading and compiling TFS..Please wait.."
 sudo git clone --recursive https://github.com/otland/forgottenserver.git
 cd forgottenserver
 sudo mkdir build && cd build
@@ -26,7 +26,7 @@ sudo make
 sudo mv tfs ..
 cd ..
 sudo mysql -u otsuser -p otsdb < schema.sql
-echo "Done! Make sure to adjust configlua before starting the server."
-echo "The following dummy user has been created at MySQL localhost:"
-echo "MySQL username: otsuser pw: password db: otsdb  Warning: TESTING PURPOSES ONLY"
-echo "You can launch the server from /home/username/forgottenserver using ./tfs"
+#echo "Done! Make sure to adjust configlua before starting the server."
+#echo "The following dummy user has been created at MySQL localhost:"
+#echo "MySQL username: otsuser pw: password db: otsdb  Warning: TESTING PURPOSES ONLY"
+#echo "You can launch the server from /home/username/forgottenserver using ./tfs"
