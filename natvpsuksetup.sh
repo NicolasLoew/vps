132126516ac78a75f89ca5dbdbf615276c1d076d
Nicolas Loew <ympker@web.de>
2023-01-21 21:21:01 +0100

    Update natvpsuksetup.sh

--- natvpsuksetup.sh
+++ natvpsuksetup.sh
@@ -5,15 +5,7 @@ apt-get -y install sudo
 echo 'none /run tmpfs defaults,size=64M 0 0' >> /etc/fstab
 mount -o remount /run
 df -h /run
-sudo adduser ympker
-sudo adduser ympker sudo
-su ympker
 cd
-mkdir -p ~/.ssh
-echo test >> ~/.ssh/authorized_keys
-cd .ssh
-chmod -R go= ~/.ssh
-sudo nano authorized_keys
 sudo apt-get -y install openssh-server
 
 
