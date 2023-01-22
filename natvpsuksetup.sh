d8bec61f394b4eea7c3a33a4fef11b3eb00d5acc
Nicolas Loew <ympker@web.de>
2023-01-22 20:45:43 +0100

    Update natvpsuksetup.sh

--- natvpsuksetup.sh
+++ natvpsuksetup.sh
@@ -6,6 +6,81 @@ echo 'none /run tmpfs defaults,size=64M 0 0' >> /etc/fstab
 mount -o remount /run
 df -h /run
 cd
-sudo apt-get -y install openssh-server
+     read -r -p "Do you want to adduser ympker? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+   sudo adduser ympker
+   sudo adduser ympker sudo
+   
+    printf "Done. User Ympker was added to sudoers group"
+     fi
+     
+read -r -p "Do you want to install OpenSSH Server? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+    sudo apt-get -y install openssh-server
+    sudo systemctl enable ssh
+    sudo systemctl start ssh
+    sudo systemctl status ssh
+    printf "You have successfully installed OpenSSH server. Remember to change SSH Port"
+     fi
 
+read -r -p "Do you want to restart SSH and SSHD? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+    
+    sudo service ssh restart
+    sudo service ssh status
+    sudo service sshd restart
+    sudo service sshd status
+    printf "Done."
+     fi
+read -r -p "Do you want to add your SSH Key? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+    cd
+    mkdir -p ~/.ssh
+    echo test >> ~/.ssh/authorized_keys
+    chmod -R go= ~/.ssh
+    cd .ssh
+    sudo nano authorized_keys
+    sudo service ssh restart
+    sudo service sshd restart
+    printf "Done."
+     fi
+     
+     read -r -p "Do you want to edit SSHD config? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+   sudo nano /etc/ssh/sshd_config
+   #change port
+   #add 
+   #PermitRootLogin no
+   #MaxAuthTries 3
+   #PubkeyAuthentication yes
+   #PasswordAuthentication no
+   #ChallengeResponseAuthentication no
+   
+    printf "Done."
+     fi
+      read -r -p "Do you want to restart SSH and SSHD? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+   sudo systemctl restart ssh
+   service ssh restart
+   sudo service sshd restart
+   sudo service sshd status
+   
+    printf "Done."
+     fi
+     
+   read -r -p "Do you want to purde Dropbear? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]]; then
+   sudo apt-get -y purge dropbear
+   sudo apt-get -y remove dropbear
+   
+   
+    printf "Done. Dropbear has been removed."
+     fi   
 
